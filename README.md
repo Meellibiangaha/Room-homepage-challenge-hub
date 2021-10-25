cb1a55e49b02d702b1bc1f1037b6c79d521becbe
Dima <dima-25500@mail.ru>
2021-10-25 19:14:10 +0300

    finaly

--- README.md
+++ README.md
@@ -1,56 +1,17 @@
-# Room homepage
 
-![Design preview for the Room homepage coding challenge](./design/desktop-preview.jpg)
+##  Ваши пользователи должны уметь: ##
 
+- Просмотр оптимального макета сайта в зависимости от размера экрана устройства.(нет) 
+- Просматривайте состояния наведения для всех интерактивных элементов на странице.(да) 
+- Перемещайтесь по слайдеру с помощью мыши / трекпада или клавиатуры.(да) 
+***
+#  Процесс #  
+- Html (div'ы)
+- Css (flexbox'ы)
+- JavaScript без библиотек (хочу для начала опробовать обычный js)  
+*** 
 
-**To do this challenge, you need a basic understanding of HTML, CSS and JavaScript.**
+##  Результат ## 
+![Hover effects](./screenshots/1.jpg)
 
-## The challenge
-
-Your challenge is to build out this e-commerce homepage and get it looking as close to the design as possible.
-
-You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
-
-Your users should be able to:
-
-- View the optimal layout for the site depending on their device's screen size
-- See hover states for all interactive elements on the page
-- Navigate the slider using either their mouse/trackpad or keyboard
-
---------------------------
-
- Home
-  Shop
-  About
-  Contact
-
-  Discover innovative ways to decorate
-
-  We provide unmatched quality, comfort, and style for property owners across the country. 
-  Our experts combine form and function in bringing your vision to life. Create a room in your 
-  own style with our collection and make your property a reflection of you and what you love.
-
-  Shop now
-
-  We are available all across the globe
-
-  With stores all over the world, it's easy for you to find furniture for your home or place of business. 
-  Locally, we’re in most major cities throughout the country. Find the branch nearest you using our 
-  store locator. Any questions? Don't hesitate to contact us today.
-
-  Shop now
-
-  Manufactured with the best materials
-
-  Our modern furniture store provide a high level of quality. Our company has invested in advanced technology 
-  to ensure that every product is made as perfect and as consistent as possible. With three decades of 
-  experience in this industry, we understand what customers want for their home and office.
-
-  Shop now
-
-  About our furniture
-
-  Our multifunctional collection blends design and function to suit your individual taste.
-  Make each room unique, or pick a cohesive theme that best express your interests and what
-  inspires you. Find the furniture pieces you need, from traditional to contemporary styles
-  or anything in between. Product specialists are available to help you create your dream space.
+ 
--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,60 @@
+//получаем 2-е кнопки
+//все картинки/текс и создаём "i", как индекс текущей картинки
+let left = document.querySelector('.but_left');
+let right = document.querySelector('.but_right');
+//с помощью querySelectorAll получаем массив из слайдов,
+//что мне показалось очень удобным
+let image = document.querySelectorAll('.slide');
+let h1 = document.querySelectorAll('.h1text');
+let p = document.querySelectorAll('.ptext');
+let i = 0;
 
+//идея функции заключается в добавлении и удалении display: block
+//и смене текста
+//из-за этого я не смог придумать анимацию к смене слайдов
 
-  // We are available all across the globe
+left.addEventListener("click", function () {
+    --i;
+    if (i < 0) {
+        image[i+1].classList.remove("block");
+        h1[i+1].classList.add("hide");
+        p[i+1].classList.add("hide");
 
-  // With stores all over the world, it's easy for you to find furniture for your home or place of business. 
-  // Locally, we’re in most major cities throughout the country. Find the branch nearest you using our 
-  // store locator. Any questions? Don't hesitate to contact us today.
+        i = 2;
 
+        image[i].classList.add("block");
+        h1[i].classList.remove("hide");
+        p[i].classList.remove("hide");
+    } else {
+        h1[i+1].classList.add("hide");
+        p[i+1].classList.add("hide");
+        image[i+1].classList.remove("block");
 
+        image[i].classList.add("block");
+        h1[i].classList.remove("hide");
+        p[i].classList.remove("hide");
+    }
+})
 
-  // Manufactured with the best materials
+right.addEventListener("click", function () {
+    ++i;
+    if (i >= image.length) {
+        image[i-1].classList.remove("block");
+        h1[i-1].classList.add("hide");
+        p[i-1].classList.add("hide");
 
-  // Our modern furniture store provide a high level of quality. Our company has invested in advanced technology 
-  // to ensure that every product is made as perfect and as consistent as possible. With three decades of 
-  // experience in this industry, we understand what customers want for their home and office.
+        i = 0;
 
+        image[i].classList.add("block");
+        h1[i].classList.remove("hide");
+        p[i].classList.remove("hide");
+    } else {
+        h1[i-1].classList.add("hide");
+        p[i-1].classList.add("hide");        
+        image[i-1].classList.remove("block");
 
-let left = document.getElementById('but_left');
-let right = document.getElementById('but_right');
-
-left.addEventListener("click", function (){ slider();});
-right.addEventListener("click", function (){ sslider();});
-
-function slider(){
-
-}
-
+        image[i].classList.add("block");
+        h1[i].classList.remove("hide");
+        p[i].classList.remove("hide");
+    }
+})
